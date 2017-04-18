14ef5f41238bbe5dd452368ad9b8aefc77dd77e2
Charlie Li <ziqiaocharlie@gmail.com>
2017-04-17 20:22:23 -0400

    Added 3 routes to the api description.

---
+++ backend/api/controllers/chat_polygons.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let util = require('util');
+
+// Export the public interface for swagger
+module.exports = {
+  getChatPolygonsWithinBounds: _getChatPolygonsWithinBounds
+};
+
+function _getChatPolygonsWithinBounds() {
+
+}
